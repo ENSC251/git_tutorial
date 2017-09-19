4bb0b8c2122e9be6a88d9942f1e4dc0eeb430ccb
knishibe <knishibe@sfu.ca>
2017-09-19 15:27:07 -0700

    Add hello executable

---
+++ git_tutorial/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>  
+using namespace std; 
+
+	
+int main()
+{
+	cout << "Hello!" <<  endl;
+	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+	cout << "Kira" << endl; //this is my contribution (knishibe@sfu.ca)
+}
