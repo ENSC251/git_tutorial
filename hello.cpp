41d763ee2db391c15fc6d9f392499f235acc8488
theknuckle <ash6234@live.ca>
2017-09-19 15:11:43 -0700

    Added newlines in hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -6,4 +6,6 @@ int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+	cout << "It is amazing how I could get a copy and create a branch using git" << endl; //my contributions
+	cout << "Great Job Akaash";															  //are these lines
 }
