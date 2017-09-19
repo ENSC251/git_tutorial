33fd4fc7dc38ebe7f62bfbdc71d2b6c0db4c730e
scottbernard <scott.bernard@hotmail.ca>
2017-09-19 14:54:29 -0700

    Add hello executable with correct output

--- hello.cpp
+++ hello.cpp
@@ -6,5 +6,5 @@ int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
-	cout << "I have added code to the hello.cpp file" << endl;
+	cout << "My name is Scott Bernard" << endl; //Added by Scott Bernard 9/19/2017
 }
