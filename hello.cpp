94c4fc8eaab4d8a932f5d0065d67d125bb803779
DelongLi <delongl@sfu.ca>
2017-09-19 16:20:45 -0700

    Money

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main()
 {
 	cout << "Hello!" <<  endl;
+	cout << " I spend $300 in Taka's sushi " <<endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
 }
+
