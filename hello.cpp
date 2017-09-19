818ea60bef467b82fce0c3cf85987a7a84133229
zzqu <john826qu@yahoo.ca>
2017-09-19 16:41:46 -0700

    add hello 3

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,6 @@ using namespace std;
 // zi zhou qu 
 int main()
 {
-	cout << "Zi Zhou Qu" <<  endl;
+	cout << "hello!\nZi Zhou Qu" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
 }
