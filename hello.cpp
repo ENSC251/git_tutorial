72504e062f2b4ff2b6afa9151778a23451b54dff
gsa76 <gsa76@ensc-esil-06.ensc.sfu.ca>
2017-09-19 15:38:54 -0700

    Modified code to print my name

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,10 @@
-#include <iostream>  
-using namespace std; 
-
-	
+#include <iostream>
+using namespace std;
 int main()
 {
-	cout << "Hello!" <<  endl;
-	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+cout << "Hello!" << endl;
+cout << "Great, you managed to get a copy of the repository from GitHub" << endl;
+ cout<<"hello world"<<endl;
+ cout<<"this was a good experience"<<endl;
+
 }
