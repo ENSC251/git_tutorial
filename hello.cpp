121370bd8b650c02247bc07b0eecaa1a74858bab
juliakim715 <jka183@sfu.ca>
2017-09-19 16:38:23 -0700

    Add hello executable

--- hello.cpp
+++ hello.cpp
@@ -7,4 +7,5 @@ int main()
 
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+	cout << "Julia" << endl; //this is my contribution jka183
 }
