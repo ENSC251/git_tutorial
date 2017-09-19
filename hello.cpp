36f20ef7401b738f6766d323912af08a8b6e3a29
juliakim715 <32074024+juliakim715@users.noreply.github.com>
2017-09-19 15:12:30 -0700

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -4,14 +4,7 @@ using namespace std;
 	
 int main()
 {
-/*
-* This class definition (or function) is derived
-* from the code available at the following location:
-* [https://github.com/SFU-2017-3-ensc251/git_tutorial]
-*The file has the following copyright from the original author:
-[This was a class tutorial. No specific copyright provided from the author]
-**/
+
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
-	cout << "Julia" << endl; //my contribution to print out my name
 }
