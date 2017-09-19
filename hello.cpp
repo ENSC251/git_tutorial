e02bd7917b166c5abb6f7ca792e029e988166e20
jnaorbe <jnaorbe@sfu.ca>
2017-09-19 15:51:23 -0700

    Add hello executable

--- hello.cpp
+++ hello.cpp
@@ -6,4 +6,9 @@ int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+	cout << "Use 'git branch' to list branches and 'git checkout -b 'nameforbranch' to make a new one"<< endl;
+	cout << " 'git clone' will get a copy of a remote repository'"<< endl;
+	cout << " Use 'cd' then directory name to change the working directory";
+	cout << "use 'make' then enter then ./filename"
+
 }
