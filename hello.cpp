390c95c6424fe1ce74240df10c9a65bdb8f3f24f
nleesfu <nelee@sfu.ca>
2017-09-19 15:07:57 -0700

    Add hello executable

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main()
 {
 	cout << "Hello!" <<  endl;
+        cout << "How are you?" << endl;
+        cout << "Bad? Aww why's that?" << endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
 }
