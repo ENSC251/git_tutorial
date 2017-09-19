fe64a1d59d97f190e40a6c86be7127a93ff0891d
MatthewMarinets <matthew.marinets@gmail.com>
2017-09-19 14:54:53 -0700

    Added another cout line to hello.cpp and hello executable

--- hello.cpp
+++ hello.cpp
@@ -6,4 +6,5 @@ int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+	cout << "And you even managed to make a new branch and edit the code :D\n";
 }
