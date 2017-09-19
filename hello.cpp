646cf899beb700938b089093063d259b82b78292
sungurean <sungurea@sfu.ca>
2017-09-19 15:02:20 -0700

    Add hello executable

--- hello.cpp
+++ hello.cpp
@@ -8,5 +8,5 @@ int main()
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
 	cout << "Stefan Ungurean's code" << endl;
 	cout << "Linux is interesting" << endl;
-	cout << " Hey Hey hey" ;
+	cout << " Hey Hey hey!!!!!, Phi kap is life" ;
 }
