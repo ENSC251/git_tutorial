0d03b61ceb16f88a341ceb5456ea2d401d9a7bcc
taylortraviss <ttraviss@sfu.ca>
2017-09-19 15:55:50 -0700

    Added name

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,8 @@ using namespace std;
 	
 int main()
 {
-	cout << "Hello!" <<  endl;
+	// I added my name to the file
+
+	cout << "Hello! My name is Taylor Traviss" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
 }
