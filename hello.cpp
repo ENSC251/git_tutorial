9e7165383e98a1c91e99acaa5420f0ef2735cb44
jamylj <32109236+jamylj@users.noreply.github.com>
2017-09-19 12:39:29 -0700

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -6,5 +6,5 @@ int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
-	cout << "I can count to potato and spell 14 numerically" << endl;
+	cout << "I can count to potato and spell 14 numerically!" << endl;
 }
