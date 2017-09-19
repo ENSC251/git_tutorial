3456bb4c1da1a5d3f3adc5eb063bdbebe562791c
zzqu <john826qu@yahoo.ca>
2017-09-19 16:01:03 -0700

    Add hello executable

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,6 @@ using namespace std;
 	
 int main()
 {
-	cout << "Hello!" <<  endl;
+	cout << "Zi Zhou Qu" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
 }
