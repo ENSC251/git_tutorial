eff1bfd09c93b865174b244f79571163f2db6a7e
eca83 <eca83@sfu.ca>
2017-09-20 20:33:10 -0700

    eca83 - new line

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,12 @@
+//contributed the last cout statement in the code
+//Emily Chen, September 20th 2017
+
 #include <iostream>  
 using namespace std; 
-
 	
 int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+	cout << "My name is Emily Chen"  << endl;
 }
