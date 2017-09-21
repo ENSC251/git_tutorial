1a87d31c044fa3edcff14f7564f8a80f1e4cbd2d
dariusnad <dnadem@sfu.ca>
2017-09-20 19:04:24 -0700

    Add hello executable

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main()
 {
 	cout << "Hello!" <<  endl;
+	cout << "Darius Nadem" << endl; //Contribution of adding name 
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
 }
