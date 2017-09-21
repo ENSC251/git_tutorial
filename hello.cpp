261d669a10ccac8c771577158cdfa7b825929beb
Taha98 <Tahaa@sfu.ca>
2017-09-21 14:54:11 -0700

    hello

--- hello.cpp
+++ hello.cpp
@@ -6,4 +6,7 @@ int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+	cout << "My name is Taha" << endl;
+
+// I have edited the file so that it prints my name when run
 }
