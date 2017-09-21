c9b9e4bd6b2ca20b5936b9cc4f2f61088d2354c8
JohnAlvarado <jpalvara@sfu.ca>
2017-09-20 23:23:46 -0700

    Added bye and name

--- hello.cpp
+++ hello.cpp
@@ -6,5 +6,6 @@ int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Hi my name is John Alvarado" << endl;
+	cout << "Bye." << endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
 }
