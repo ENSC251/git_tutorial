6211af8956719ef891b0cfb966e96f0638871e5f
kielhenkelman <kielhenkelman@gmail.com>
2017-09-21 14:39:45 -0700

    I am making a commitment

--- hello.cpp
+++ hello.cpp
@@ -6,4 +6,6 @@ int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+	cout << "I am also capable of adding lines of code to this program! Sweet!" <<  endl;
+	cout << "Testing 123" <<  endl;
 }
