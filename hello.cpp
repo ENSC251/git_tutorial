1a695f9a4d9743c6623c3f15b93cfff8c1d003c2
Joseph Dillman <jdillman@sfu.ca>
2017-09-22 13:10:25 -0700

    Add hello executable

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,7 @@ using namespace std;
 	
 int main()
 {
-	cout << "Hello!" <<  endl;
-	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+	cout << "this is a git branch for ENSC 251" <<  endl;
+	cout << "-Joseph Dillman, jdillman@sfu.ca, 301317623";
+
 }
