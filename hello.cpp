0404f8bd7f3ee31d02ca75399e7b641d8b3d9029
ksackvil <sackvillehiik@gmail.com>
2017-09-21 19:50:03 -0700

    Added hello exe

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main()
 {
 	cout << "Hello!" <<  endl;
-	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+	cout << "I did my homework! " <<  endl;
+	cout << "If you have two cookies and I eat 4 halfs of it, you will have no cookie." << endl; 
 }
