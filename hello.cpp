29deaa624a06d4ee98b99ccb3b8afae9de9f95c0
spaulsen <spaulsen@sfu.ca>
2017-09-22 15:54:08 -0700

    hello

--- hello.cpp
+++ hello.cpp
@@ -6,4 +6,7 @@ int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+	
+	//The following line of code was added by Sean Paulsen
+	cout << "Sean Paulsen got a copy of repository\n";
 }
