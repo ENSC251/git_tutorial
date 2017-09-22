cdbf9ecac72c11bcc9a3d8faa11312218a95bdf7
Spenny1068 <spncrlll838@gmail.com>
2017-09-22 09:58:44 -0700

    add hello excecutable

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,8 @@ using namespace std;
 	
 int main()
 {
+	cout << "hello my name is spencer";
+	cout << "im done";
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
 }
