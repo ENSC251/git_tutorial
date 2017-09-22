fe91d9405b21eb6da6439aaceaba72eae2d53ff1
tcl18 <32207620+tcl18@users.noreply.github.com>
2017-09-22 13:46:32 -0700

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -6,5 +6,5 @@ int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
-	cout << "Now get out" << endl;
+	cout << "Hi, this is Trevor Lee speaking" << endl; // this is the line I added
 }
