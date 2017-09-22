617b196dce2af21e6497cd0059773bf5ca4d62a2
victorlanceluz <victorlanceluz@outlook.com>
2017-09-22 09:53:43 -0700

    hello.exe

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,9 @@
 #include <iostream>  
 using namespace std; 
-
+//Victor Luz made edits
 	
 int main()
 {
-	cout << "Hello!" <<  endl;
+	cout << "Hello! Victor Luz" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
 }
