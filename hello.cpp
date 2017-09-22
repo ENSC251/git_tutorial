39622df17ae05d8819d501654c5a9c9941fad34c
yla455 <yla455@sfu.ca>
2017-09-22 10:22:43 -0700

    Add hello executable

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,12 @@
 #include <iostream>  
-using namespace std; 
 
+using namespace std; 
+/* print my name
+ */
 	
 int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+cout << "yifanliu" << endl;
 }
