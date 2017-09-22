e2e747545c74c5facb0c36ec68c30398d0c43ca7
kielhenkelman <kielhenkelman@gmail.com>
2017-09-22 09:41:42 -0700

    Used git, I hope this works

--- hello.cpp
+++ hello.cpp
@@ -6,6 +6,7 @@ int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
-	cout << "I am also capable of adding lines of code to this program! Sweet!" <<  endl;
-	cout << "Testing 123" <<  endl;
+	cout << "Kiel" <<  endl;
+	cout << "Yiwen" <<  endl;
+	// I have made a contribution, adding names, to this file.
 }
