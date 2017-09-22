2eb7925465d86b8c2057ea27f603aceddc7a3fcc
twmay <twmay@sfu.ca>
2017-09-22 10:14:35 -0700

    add text

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 	
 int main()
 {
-	cout << "random code" << endl;
+	cout << "random text" << endl;
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
 }
