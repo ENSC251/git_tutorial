3dbe9fe5ab6b281b79a6d4fe340189dfb420b847
srezghig <39532631+srezghig@users.noreply.github.com>
2018-05-22 11:50:54 -0700

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -6,5 +6,4 @@ int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
-	cout << "Hey! This is Siavash Rezghighomi" << endl;
 }
