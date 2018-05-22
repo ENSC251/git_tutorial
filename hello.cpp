0357c9b41d8de3c66c54aead38a3bc33531918b0
rislamri <rislamri@ensc-esil-12.ensc.sfu.ca>
2018-05-22 12:02:17 -0700

    Add hello executable

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main()
 {
 	cout << "Hello!" <<  endl;
-	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+	cout << "Raihan"<<endl;
+cout<< "SFU"<<endl;
+cout<< "Ensc 251"<<endl;
 }
