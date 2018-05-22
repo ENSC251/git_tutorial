4d0fda566f6acd4e1b2b8fdfccdc2ddd85f54c82
iakpan@sfu.ca <iakpan@sfu.ca>
2018-05-22 12:08:05 -0700

    Add hello executable

--- hello.cpp
+++ hello.cpp
@@ -6,4 +6,5 @@ int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+	cout << "I know this is new and a bit difficult, but you'll get the hang of it soon" << endl;
 }
