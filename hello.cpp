351bffc8eab5d7c74d3a4f473c9f3c2964cace66
Emmanueliyiola77 <ikomolaf@sfu.ca>
2018-05-22 12:03:08 -0700

    Add hello executable

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,11 @@
-#include <iostream>  
-using namespace std; 
+#include <iostream>
+using namespace std;
 
-	
+
 int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+	cout<<"how are you my name is david" <<endl;
+	cout<<" i am a student at sfu in engineering sciences"<<endl;
 }
