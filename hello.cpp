83c85caffffa304748652df0f67e25a47090ed2e
tmwinjil <takundalm@outlook.com>
2018-05-22 11:01:07 -0700

    Add hello executable

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,13 @@
-#include <iostream>  
-using namespace std; 
+#include <iostream>
+using namespace std;
 
-	
+
 int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+
+	//Modifying original code to add my name as an output
+	cout<<"Name: Takunda MWinjilo"<<endl;
 }
+
