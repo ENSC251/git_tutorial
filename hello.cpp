75b478a119a7e1086ed38dbf5cb854a706ff3608
akhabibu <akhabibu@ensc-esil-22.ensc.sfu.ca>
2018-05-22 12:08:19 -0700

    Add hello executable

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,16 @@
 #include <iostream>  
 using namespace std; 
 
-	
+// I declare that this assignment is my own work and that I have correctly acknowledged the
+// work of others. I acknowledged that I have read and followed the Academic Honesty and
+// Integrity related policies as outlined in the syllabus.
+//
+// Anvar Khabibulin
+// 05/22/2018
+// 301326035	
 int main()
 {
 	cout << "Hello!" <<  endl;
+	cout << "hello! My name is Anvar" << endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
 }
