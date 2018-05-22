4a124237415fc1ecc826ed14751cf385569faa86
Benzooka <Benjaminnotliketheothers@gmail.com>
2018-05-22 11:00:12 -0700

    Add hello executable

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,9 @@
-#include <iostream>  
-using namespace std; 
+#include <iostream>
+using namespace std;
 
-	
+
 int main()
 {
-	cout << "Hello!" <<  endl;
-	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+    //personalized the greeting to myself
+	cout << "Hello Benjamin! You have been productive today" <<  endl;
 }
