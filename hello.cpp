0f6cdc3973c1962983d2503ed4a82a24732abd86
BoyangHu233 <39578475+BoyangHu233@users.noreply.github.com>
2018-05-23 21:41:16 -0700

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main()
 {
 	cout << "Hello! Git Hub!!!" <<  endl;
+	cout << "Rookie\n";
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
 }
