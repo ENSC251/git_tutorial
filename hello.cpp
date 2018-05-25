9f774e2e713abf0cfb87c214bf8e08546deaf774
yogesh1229 <yogesh1229@hotmail.com>
2018-05-25 14:46:09 -0700

    Add hello executable

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main()
 {
 	cout << "Hello!" <<  endl;
+	cout << "Hello you've entered Yogesh's github hello world\n";
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
 }
+
