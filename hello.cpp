90edf427aa778fd68f69d376d6a62b06ea265d05
RoyAtaya <roy.ataya@gmail.com>
2018-05-25 15:24:08 -0700

    add hello executable

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,9 @@ using namespace std;
 int main()
 {
 	cout << "Hello!" <<  endl;
+    cout << "Hello!" <<  endl;
+    cout << "Hello! my name is Roy" <<  endl;
+    cout << "Hello!" <<  endl;
+    cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
 }
