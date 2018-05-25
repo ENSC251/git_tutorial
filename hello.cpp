f757c9f68b1223636964034620aa1d31dfff8e95
Nathaniel121 <nathanielhuang121@gmail.com>
2018-05-25 14:51:09 -0700

    add 123

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,6 @@ using namespace std;
 	
 int main()
 {
-	cout << "Hello!" <<  endl;
-	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+	cout << "Hello!123" <<  endl;
+	cout << "123Great, you managed to get a copy of the repository from GitHub" <<  endl;
 }
