af777250b4d4ddf88b4cb25721ccfe1fc9bb88c9
mingqit <mingqit@sfu.ca>
2018-05-25 15:59:16 -0700

    Add my name

--- hello.cpp
+++ hello.cpp
@@ -6,4 +6,6 @@ int main()
 {
 	cout << "Hello!" <<  endl;
 	cout << "Great, you managed to get a copy of the repository from GitHub" <<  endl;
+	//print my name
+	cout << "My name is Mingqi Tian" << endl;	
 }
